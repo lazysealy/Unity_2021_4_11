2b8f96e90e9d1cfb4d03bfc1e017c3bc8db9022e
lazysealy <82298507+lazysealy@users.noreply.github.com>
2021-05-02 17:01:08 +0800

    API練習

--- New Unity Project/Assets/腳本Scripts/APIStatic.cs
+++ New Unity Project/Assets/腳本Scripts/APIStatic.cs
@@ -8,6 +8,7 @@ public class APIStatic : MonoBehaviour
 
     private void Start()
     {
+        #region 認識靜態屬性的方法
         // 屬性 欄位 要知道如何存取
         // 練習取得靜態屬性 Static Properties
         // 語法
@@ -20,7 +21,34 @@ public class APIStatic : MonoBehaviour
         // 語法
         // 類別名程.靜態屬性名稱 指定 值;
         Cursor.visible = false;                // 指標.可見度
+
+        // 練習存放靜態屬性 Static Methods
+        // 語法
+        // 類別名程.靜態方法(對應參數)
+        int attack = Random.Range(100, 300);
+        print("隨機攻擊力:" + attack);
+
+        float n = Mathf.Abs(-99.9F);
+        print("絕對值結果:" + n);
+        #endregion
+
+        #region 練習
+
+        print("攝影機數量:" + Camera.allCamerasCount);
+        print("重力:" + Physics2D.gravity);
+        
+
+        #endregion
     }
 
+    private void Update()
+    {
+        #region 練習
+
+        print("玩家是否按下任意鍵:" + Input.anyKeyDown);
+        print("遊戲時間:" + Time.time);
+
+        #endregion
+    }
 
 }
