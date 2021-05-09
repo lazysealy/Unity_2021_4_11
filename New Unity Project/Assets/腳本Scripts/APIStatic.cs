66b01ceb445509a189fa38250fd8d4f232155603
lazysealy <82298507+lazysealy@users.noreply.github.com>
2021-05-09 13:02:19 +0800

    作業複習與非靜態API練習

--- New Unity Project/Assets/腳本Scripts/APIStatic.cs
+++ New Unity Project/Assets/腳本Scripts/APIStatic.cs
@@ -5,6 +5,8 @@ public class APIStatic : MonoBehaviour
     // 認識靜態 API
     // 包含關鍵字 static 就是靜態
 
+    public Vector3 a = new Vector3(1, 1, 1);
+    public Vector3 b = new Vector3(22, 22, 22);
 
     private void Start()
     {
@@ -36,19 +38,32 @@ public class APIStatic : MonoBehaviour
 
         print("攝影機數量:" + Camera.allCamerasCount);
         print("重力:" + Physics2D.gravity);
-        
+
+        Physics2D.gravity = new Vector2(0, -20);
+        print("重力:" + Physics2D.gravity);
+
+        //Application.OpenURL("https://unity.com/");
+        float f = Mathf.Floor(9.999f);
+        print("去掉小數點的結果:" + f);
+
+        float dis = Vector3.Distance(a, b);
+        print("A 與 B 的距離:" + dis);
 
         #endregion
-    }
+    } 
 
     private void Update()
     {
         #region 練習
+        // print("玩家是否按下任意鍵:" + Input.anyKeyDown);
+        // print("遊戲時間:" + Time.time);
+
+        bool b = Input.GetKeyDown("space");
+        print("是否按下空白鍵:" + b);
 
         print("玩家是否按下任意鍵:" + Input.anyKeyDown);
-        print("遊戲時間:" + Time.time);
-
+        print("遊戲時間:" + Time.time);  
         #endregion
     }
 
-}
+} 
---
+++ New Unity Project/Assets/腳本Scripts/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // 引用場景管理器API
+
+public class MenuManager : MonoBehaviour
+{
+    // 使用靜態方法處理 1. 開始遊戲 2. 離開遊戲
+    // 如何讓按鈕跟程式溝通
+    // 需要一個公開的方法
+
+    public void StearGame()
+    {
+        // 延遲呼叫 (方法名稱,"字串", 延遲時間)
+        Invoke("DelayStearGame", 1.1f);
+    }
+
+    private void DelayStearGame()
+    {
+        // Application.LoadLevel("遊戲畫面"); //綠色蚯蚓 : 過時的 API 建議換新
+
+        // 場景管理.載入場景("場景名稱")
+        SceneManager.LoadScene("遊戲畫面");  // 使用【字串】載入
+        // SceneManager.LoadScene(1);       // 使用【編號】載入
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+        Invoke("DelayQuitGame", 1.1f);
+    }
+
+    private void DelayQuitGame()
+    {
+        // 應用程式.離開()
+        Application.Quit();
+    }
+}
